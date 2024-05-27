ee71eb1cb2b4b100f2819811097b184974b3efa5
iamparth2003 <147431202+iamparth2003@users.noreply.github.com>
2024-05-27 10:01:22 +0530

    Add files via upload

---
+++ inheritanceExample/src/inheritanceExample/DemoChild.java
@@ -0,0 +1,10 @@
+package inheritanceExample;
+
+public class DemoChild {
+	public static void main(String args[]) {
+		ChildClass ch = new ChildClass();
+		
+		ch.setAmt(12000);
+		System.out.println(ch);
+	}
+}
---
+++ inheritanceExample/src/inheritanceExample/NextChild.java
@@ -0,0 +1,8 @@
+package inheritanceExample;
+
+public class NextChild {
+public static void main(String args[])
+{
+	NextChild nc  = new NextChild();
+}
+}
