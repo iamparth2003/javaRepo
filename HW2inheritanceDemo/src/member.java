d5579fb86f66094c44e6ba579c4e7dd9d00a8631
iamparth2003 <147431202+iamparth2003@users.noreply.github.com>
2024-05-29 18:45:45 +0530

    Add files via upload

---
+++ HW2inheritanceDemo/src/member.java
@@ -0,0 +1,43 @@
+
+public class member {
+	String Name,address;
+    int age,phone,salary;
+	public member(String name, String address, int age, int phone, int salary) {
+		Name = name;
+		this.address = address;
+		this.age = age;
+		this.phone = phone;
+		this.salary = salary;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+}
